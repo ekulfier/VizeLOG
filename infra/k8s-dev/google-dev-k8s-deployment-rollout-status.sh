79d00a69c6a6cd8da11d00b5d1c6131230710289
sorrachak.holy201 <sorrachak.holy201@gmail.com>
2024-02-08 03:48:24 +0700

    fix: pipeline

--- infra/k8s-dev/google-dev-k8s-deployment-rollout-status.sh
+++ infra/k8s-dev/google-dev-k8s-deployment-rollout-status.sh
@@ -3,15 +3,12 @@
 
 #k8s-deployment-rollout-status.sh
 
-curl -LO "https://storage.googleapis.com/kubernetes-release/release/v1.20.5/bin/linux/amd64/kubectl"
-chmod u+x ./kubectl
-
 sleep 60s
 
-if [[ $(./kubectl -n dev rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]]; 
+if [[ $(kubectl -n dev rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]]; 
 then     
 	echo "Deployment ${deploymentName} Rollout has Failed"
-    ./kubectl -n dev rollout undo deploy ${deploymentName}
+    kubectl -n dev rollout undo deploy ${deploymentName}
     exit 1;
 else
 	echo "Deployment ${deploymentName} Rollout is Success"
--- infra/k8s-dev/google-dev-k8s-deployment.sh
+++ infra/k8s-dev/google-dev-k8s-deployment.sh
@@ -3,14 +3,8 @@
 
 #k8s-deployment.sh
 
-curl -LO "https://storage.googleapis.com/kubernetes-release/release/v1.20.5/bin/linux/amd64/kubectl"
-
-sleep 20s
-
-# Make it executable
-chmod +x ./kubectl
-
 sed -i "s#replace#${imageName}#g" google-depl-dev.yaml
 
-./kubectl -n dev apply -f google-depl-dev.yaml
+kubectl -n dev apply -f google-depl-dev.yaml
+
 ######################### update existing k8s-deployment.sh #########################
